5d426d50613d9a73a55cd736de1d988528845854
Coze Agent <coze@coze.cn>
2026-07-16 10:41:32 +0800

    refactor: 包名 org.springblade.modules.iot.eiot 修改为 org.springblade.modules.iot
    - 移动 325 个 Java 文件从 eiot/ 子目录到上级目录
    - 更新所有 package 声明和 import 语句（27 个模块）
    - 更新 ES mapper scan 路径配置
    - 涉及模块：iot-components（8个）、module-iot-core（12个）、module-iot-task、module-iot-temporal（6个）
    
    Coze-Commit-Type: user
    Coze-User-ID: 1751938984068412
    Coze-Conversation-ID: 8983462

--- iot-platform/module-iot-temporal/iot-temporal-serviceimpl-es/src/main/java/org/springblade/modules/iot/temporal/es/config/EasyEsSpringConfig.java
+++ iot-platform/module-iot-temporal/iot-temporal-serviceimpl-es/src/main/java/org/springblade/modules/iot/temporal/es/config/EasyEsSpringConfig.java
@@ -16,7 +16,7 @@ import java.util.Map;
  * @author lzh
  * @since 2.1.0
  */
-@EsMapperScan("${platform.info.base-package}.eiot")
+@EsMapperScan("${platform.info.base-package}")
 @ComponentScan("org.dromara.easyes")
 @Configuration
 public class EasyEsSpringConfig {
