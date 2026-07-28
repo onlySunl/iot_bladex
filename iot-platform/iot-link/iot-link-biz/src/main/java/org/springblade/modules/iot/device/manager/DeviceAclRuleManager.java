991c0cb64d0daac80b81c901d63adf6b142088bd
孙林 <305530395@qq.com>
2026-07-28 18:37:17 +0800

    删除 link rule 两个模块

--- iot-platform/iot-link/iot-link-biz/src/main/java/org/springblade/modules/iot/device/manager/DeviceAclRuleManager.java
+++
@@ -1,8 +0,0 @@
-package org.springblade.modules.iot.device.manager;
-
-/**
- * 设备访问控制(ACL)规则 ── 数据访问层。
- * 缓存逻辑全部下沉到 LinkCacheDataHelper + DeviceAclRuleCacheService。
- */
-public interface DeviceAclRuleManager extends BaseService<DeviceAclRule> {
-}
